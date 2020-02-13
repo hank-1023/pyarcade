b531af2b761cff4bec39f5d86c5db61a44f5a068
Zizhou Wang <hank2u@icloud.com>
2020-02-13 10:44:49 -0500

    Created function outline for parse_input

---
+++ pyarcade/input_system.py
@@ -0,0 +1,11 @@
+def parse_input(user_input: str):
+    if user_input == "reset":
+        # reset the game
+        pass
+    elif user_input == "clear":
+        # clear the game
+        pass
+    else:
+        for u_input in user_input:
+            if u_input.isdigit():
+                pass
