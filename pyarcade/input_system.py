20d95f5858475666a5a0b5fa194b5055b077c962
Zizhou Wang <hank2u@icloud.com>
2020-02-14 20:58:37 -0500

    some minor debugging

--- pyarcade/input_system.py
+++ pyarcade/input_system.py
@@ -1,5 +1,5 @@
 from pyarcade.mastermind import Mastermind
-import sys
+from sys import stdin
 
 
 class Client:
@@ -10,21 +10,25 @@ class Client:
 
     def start(self):
         print("Please start guessing: ")
-        for line in sys.stdin:
+        while True:
+            line = stdin.readline().rsplit()[0]
             result_code, result_arr = self.input_manager.parse_input(line)
             if result_code == 2:
-                self.master_mind.on_made_guess(result_arr)
+                print(self.master_mind.on_made_guess(result_arr))
 
 
 class InputManager:
-    """ A class managing all user input """
+    """ A class managing all user input
+
+        Returns: (int, [int]) as result code and parsed array (if any)
+    """
     def parse_input(self, user_input: str) -> (int, [int]):
         if user_input == "reset":
             return 0, []
         elif user_input == "clear":
             return 1, []
         else:
-            if user_input.isnumeric():                      # Checking if all digits in input string are digits
+            if user_input.isnumeric():   # Checking if all digits in input string are 4 digits
                 int_array = [int(i) for i in user_input]
                 return 2, int_array
             else:
