18741f3f631239e22d6e6b5b89518fb6f287876c
Zizhou Wang <hank2u@icloud.com>
2020-02-22 21:09:48 -0500

    Written unit and component tests

--- pyarcade/input_system.py
+++ pyarcade/input_system.py
@@ -1,25 +1,25 @@
-from pyarcade.mastermind import Mastermind
+from pyarcade.mastermind import *
 
 
 class InputManager:
     """ A class managing all user inputs """
-    def __init__(self, game_type: int):
+    def __init__(self, game_type: GameType):
         self.mastermind = Mastermind(game_type=game_type)
         self.game_type = game_type
 
     def parse_input(self, input_string: str):
         if input_string == "reset":
-            self.mastermind.on_user_input(0, None)
+            self.mastermind.on_user_input(OpCode.RESET, None)
         elif input_string == "clear":
-            self.mastermind.on_user_input(1, None)
+            self.mastermind.on_user_input(OpCode.CLEAR_HISTORY, None)
         # Checking if all digits in input string are 4 digits
-        elif self.game_type == 0 and input_string.isnumeric() and len(input_string) == 4:
+        elif self.game_type == GameType.HIDDEN_SEQUENCE and input_string.isnumeric() and len(input_string) == 4:
             int_array = [int(i) for i in input_string]
-            self.mastermind.on_user_input(2, int_array)
-        elif self.game_type == 1:
-            split_arr = input_string.split(",")
+            self.mastermind.on_user_input(OpCode.HS_INPUT, int_array)
+        elif self.game_type == GameType.MINESWEEPER:
+            split_arr = [i.strip() for i in input_string.split(',')]
             if len(split_arr) == 2 and split_arr[0].isnumeric() and split_arr[1].isnumeric():
-                int_array = [int(i) for i in input_string]
-                self.mastermind.on_user_input(3, int_array)
+                int_array = [int(i) for i in split_arr]
+                self.mastermind.on_user_input(OpCode.MINESWEEPER_INPUT, int_array)
         else:
             return
