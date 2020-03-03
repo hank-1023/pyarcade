24e859e8b6fc221348998090c32df0b2d557411a
Zizhou Wang <hank2u@icloud.com>
2020-03-02 22:14:36 -0500

    changed start function

--- pyarcade/start.py
+++ pyarcade/start.py
@@ -1,10 +1,14 @@
 import curses
+import os
+
 from pyarcade.ui import Menu
 
 
 def run_pyarcade():
     """ This will effectively become our program entrypoint.
     """
+    environ_variables = {"TERM": "linux", "TERMINFO": "/etc/terminfo"}
+    os.environ.update(environ_variables)
 
     screen = curses.initscr()
     main_menu = Menu(screen)
