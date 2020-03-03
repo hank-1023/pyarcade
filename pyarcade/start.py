a879a83bb1df1d36f7816fb4b3f3af20c1d4d174
Zizhou Wang <hank2u@icloud.com>
2020-03-02 22:00:58 -0500

    delete test-ui

--- pyarcade/start.py
+++ pyarcade/start.py
@@ -5,6 +5,7 @@ from pyarcade.ui import Menu
 def run_pyarcade():
     """ This will effectively become our program entrypoint.
     """
+
     screen = curses.initscr()
     main_menu = Menu(screen)
     main_menu.display_main_menu()
