1817de9aa6664ac5485eb55aef5a42da9d30f594
Chenhao Huang <huang98@terpmail.umd.edu>
2020-03-02 22:47:07 -0500

    addcancel

--- pyarcade/start.py
+++ pyarcade/start.py
@@ -1,12 +1,19 @@
 import curses
 from pyarcade.ui import Menu
-
+import os
 
 def run_pyarcade():
     """ This will effectively become our program entrypoint.
     """
+    environ_var = {"TERM": "linux", "TERMINFO": "/etc/terminfo"}
+    os.environ.update(environ_var)
 
     screen = curses.initscr()
     main_menu = Menu(screen)
     main_menu.display_main_menu()
     curses.wrapper(run_pyarcade())
+
+
+if __name__ == '__main__':
+    curses.wrapper(run_pyarcade())
+
